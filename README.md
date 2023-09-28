f9b129e0fe510d5fe6f775d5b0ab65fc87594c90
tobii-dev <77458451+tobii-dev@users.noreply.github.com>
2023-09-28 21:02:28 +0200

    baby steps rpc

--- README.md
+++ README.md
@@ -1,7 +1,71 @@
 # blur-lua-discord-rpc
 Discord Rich Presence for Blur, but as a Lua plugin, but implemented in Rust, but loaded as a Lua module.
 
+
+## Build
+
 ```shell
 LUA_LIB_NAME=lib/lua5.1 LUA_LINK=cdylib cargo build --release --target=i686-pc-windows-msvc
 cp "target/i686-pc-windows-msvc/release/blur_discord_rpc.dll" "<BLUR_DIR>/amax/plugins/discord_rpc/lib-blur_discord_rpc.dll"
+```
+
+
+## Lua plugin
+
+`amax/plugins/discord_rpc/blur_discord_rpc.lua`:
+
+```lua
+local M = {
+	lib = nil,
+	client = nil,
+	info = {
+		name = "Dicord_RPC",
+		description = "Discord Rich Presence Client for Blur",
+		version = 0.1,
+	},
+}
+
+
+function M.update_rpc(new_rpc)
+	print_api("update_rpc(" .. new_rpc .. ")")
+	M.client:update_rpc(new_rpc)
+end
+
+
+function M.update_rpc_pcall(new_rpc)
+	if not M.client then
+		return
+	end
+	local client = M.client
+	print_api("update_rpc_pcall(" .. new_rpc .. ")")
+	local f = function (x)
+		client:update_rpc(x)
+	end
+	local ok, rs = pcall(f, name_next)
+	if ok then
+		print_api("updated RPC :)")
+	else
+		print_api("NOT OK :( error: " .. tostring(rs))
+	end
+end
+
+
+-- This will be called when the plugin loads
+function M.init()
+	M.lib = require("discord_rpc.lib-blur_discord_rpc")
+	local client = M.lib.start()
+	if not client
+		return false
+	end
+	M.client = client
+	return true -- true means we loaded correctly
+end
+
+
+-- This will be called when the plugin unloads (game closes)
+function M.free()
+	return M.client.stop()
+end
+
+return M -- Return this plugin
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,143 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::sync::mpsc::{Receiver, Sender};
 
 use mlua::prelude::*;
+use mlua::UserData;
 
-fn init(l: &Lua, _: ()) -> LuaResult<bool> {
-	let log_file = PathBuf::from(".")
-		.join("amax")
-		.join("log")
-		.join("blur_lua_discord_rpc.log");
-	let Ok(mut f) = File::create(log_file) else {
-		return Ok(false);
+
+
+struct LuaDiscordClient {
+	tx: Sender<String>,
+}
+
+impl LuaDiscordClient {
+	fn new() -> Self {
+		let (tx, rx) = std::sync::mpsc::channel::<String>();
+		std::thread::spawn(|| {
+			client_thread(rx);
+		});
+		LuaDiscordClient { tx }
+	}
+}
+
+impl UserData for LuaDiscordClient {
+	// fn add_fields<'lua, F: LuaUserDataFields<'lua, Self>>(_fields: &mut F) {}
+
+	fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
+
+		//TODO: client:update_rpc()
+		// - How to handle bad starts?
+		methods.add_method("update_rpc", |_, this, state: String| -> LuaResult<()> {
+			log::info!("update_rpc( {state} )");
+			if this.tx.send(state).is_err() {
+				log::info!("tx.send failed");
+				return Ok(());
+			}
+			log::info!("sent update_rpc");
+			Ok(())
+		});
+
+		//TODO: client:stop() method
+		methods.add_method("stop", |_, this, _: ()| -> LuaResult<()> {
+			this.tx.send("stfu".to_string()).unwrap();
+			Ok(())
+		});
+
+
+		//TODO: client:is_online() method
+		methods.add_method("is_online", |_, _this, _: ()| -> LuaResult<bool> {
+			Ok(false)
+		});
+
+	}
+}
+
+//TODO: Move Discord stuff to rpc.rs
+fn client_thread(rx: Receiver<String>) {
+	use discord_rich_presence::{activity::{Assets, self}, DiscordIpc, DiscordIpcClient};
+	const CLIENT_ID: u64 = 933390770453499974;
+	let logo = Assets::new().large_image("logo");
+	let Ok(mut client) = DiscordIpcClient::new(&CLIENT_ID.to_string()) else {
+		log::info!("Failed to DiscordIpcClient::new()");
+		return;
 	};
-	let x = l.used_memory();
-	let Ok(_) = write!(
-		f,
-		"Hi from Blur_Discord_Rich_Presence_Client! Used memory: {x} bytes."
-	) else {
-		return Ok(false);
+	log::info!("Created client");
+	let mut connected = if client.connect().is_ok() {
+		client
+			.set_activity(
+				activity::Activity::new()
+					.state("BLUR_STATE")
+					.details("BLUR_DETAILS")
+					.assets(Assets::new().large_image("logo")),
+			)
+			.unwrap();
+		true
+	} else {
+		log::info!("client.connect() failed");
+		false
 	};
-	Ok(true)
+	log::info!("Created activity...");
+	while let Ok(state) = rx.recv() {
+		log::info!("rx STATE: {state}");
+		if !connected {
+			log::info!("trying to reconnect");
+			let r = client.connect();
+			connected = r.is_ok();
+		}
+		if !connected {
+			log::info!("failed to reconnect");
+			continue;
+		}
+
+		if client
+			.set_activity(
+				activity::Activity::new()
+					.state(&state)
+					.assets(Assets::new().large_image("logo")),
+			)
+			.is_err()
+		{
+			log::info!("set_activity error");
+			continue;
+		}
+		log::info!("updated rpc to {state}");
+	}
 }
 
 #[mlua::lua_module]
 fn blur_discord_rpc(lua: &Lua) -> LuaResult<LuaTable> {
+	init_logs();
+
 	let exports = lua.create_table()?;
 
-	let info = lua.create_table()?;
-	info.set("name", "Blur_Discord_Rich_Presence_Client")?;
-	info.set("version", 0.1)?;
-	exports.set("info", info)?;
-
-	exports.set("init", lua.create_function(init)?)?;
+	fn start(l: &Lua, _: ()) -> LuaResult<LuaDiscordClient> {
+		let mem = l.used_memory();
+		dbg!(mem);
+		let client = LuaDiscordClient::new();
+		Ok(client)
+	}
+	exports.set("start", lua.create_function(start)?)?;
 	Ok(exports)
+}
+
+fn init_logs() {
+	use log::LevelFilter;
+	use simplelog::{
+		ColorChoice, CombinedLogger, Config, ConfigBuilder, TermLogger, TerminalMode, WriteLogger,
+	};
+	let cfg = ConfigBuilder::new()
+		.set_time_offset_to_local()
+		.unwrap()
+		.build();
+	let log_file = blur_plugins_core::create_log_file("blur_discord_rpc.log").unwrap();
+	CombinedLogger::init(vec![
+		TermLogger::new(
+			LevelFilter::Trace,
+			cfg,
+			TerminalMode::Mixed,
+			ColorChoice::Auto,
+		),
+		WriteLogger::new(LevelFilter::Trace, Config::default(), log_file),
+	])
+	.unwrap();
+	log_panics::init();
 }
